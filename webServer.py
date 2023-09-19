b443cd0e3d74dad3598e05b1101f322ceee72d8b
ks5448 <ks5448@nyu.edu>
2023-09-19 19:23:22 -0400

    9

--- webServer.py
+++ webServer.py
@@ -56,9 +56,9 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
       connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n")
-      connectionSocket.send(b"404 Not Found - File Not Found")
+      connectionSocket.send(b"404 Not Found - File Not Found\r\n")
       connectionSocket.send(b"\r\n")
       print("Error 404 - File Not Found", e)
       #Fill in end
